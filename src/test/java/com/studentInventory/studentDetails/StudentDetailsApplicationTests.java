0d9306549c41c17b8c693cf9dc69013a61a65562
Muthubashini Sathsara <muthubashinisathsara@gmail.com>
2024-12-18 13:53:40 +0530

    push to main branch

---
+++ src/test/java/com/studentInventory/studentDetails/StudentDetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentInventory.studentDetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentDetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
